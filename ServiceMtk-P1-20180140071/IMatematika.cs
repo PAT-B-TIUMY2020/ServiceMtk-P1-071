bfeeacba8cbae5b5bd316b4c2c5efc10fa999f3b
AisyahDiniaKencana <56532105+AisyahDiniaKencana@users.noreply.github.com>
2020-11-07 14:27:13 +0700

    add exception

---
+++ ServiceMtk-P1-20180140071/IMatematika.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceMtk_P1_20180140071
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IMatematika
+    {
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exeption
+        /* output->*/
+        int Tambah(int a, int b); //method
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exeption
+        int Kurang(int a, int b); //input
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exeption
+        int Kali(int a, int b);
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exeption
+        int Bagi(int a, int b);
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))] //membuat kelas yang melambangkan exeption
+        Koordinat TKoordinat(Koordinat a, Koordinat b); //object dari class
+    }
+
+    
+ [DataContract]
+ public class Koordinat
+    {
+        private int _x, _y; //atribut
+        [DataMember]//membuka koordinat, x,y
+        public int X //properties
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+        [DataMember]
+        public int Y //properties
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+    }
+
+    [DataContract]
+    class MathFault
+    {
+        [DataMember]
+        public string Kode { get; set; }
+        [DataMember]
+        public string Pesan { get; set; }
+    }
+}
---
+++ ServiceMtk-P1-20180140071/Matematika.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceMtk_P1_20180140071
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Matematika : IMatematika
+    {
+        public int Bagi(int a, int b)
+        {
+            try
+            {
+                return a / b;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //secara default
+                //throw new Exception(ex.Message) // pesan error secara default
+                //throw new FaultException("pesan salah") // untuk meloloskan pesan yang dikirim ke client
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Input yang Anda Masukan Salah";
+                mf.Pesan = "Masukan Input yang Benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+        public int Kali(int a, int b)
+        {
+            try
+            {
+                return a * b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //secara default
+                //throw new Exception(ex.Message) // pesan error secara default
+                //throw new FaultException("pesan salah") // untuk meloloskan pesan yang dikirim ke client
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Input yang Anda Masukan Salah";
+                mf.Pesan = "Masukan Input yang Benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+        public int Kurang(int a, int b)
+        {
+            try
+            {
+                return a - b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //secara default
+                //throw new Exception(ex.Message) // pesan error secara default
+                //throw new FaultException("pesan salah") // untuk meloloskan pesan yang dikirim ke client
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Input yang Anda Masukan Salah";
+                mf.Pesan = "Masukan Input yang Benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+        public int Tambah(int a, int b)
+        {
+            try
+            {
+                return a + b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //secara default
+                //throw new Exception(ex.Message) // pesan error secara default
+                //throw new FaultException("pesan salah") // untuk meloloskan pesan yang dikirim ke client
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Input yang Anda Masukan Salah";
+                mf.Pesan = "Masukan Input yang Benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+        //TKoordinat:nama methodnya, a,b = input seperti int.
+        //Membuat koordinat hasil lalu menjumlahkan xa dengan xb, ya
+        
+ public Koordinat TKoordinat(Koordinat a, Koordinat b)
+        {
+            try
+            {
+                Koordinat hasil = new Koordinat();
+                hasil.X = a.X + b.X;
+                hasil.Y = a.Y + b.Y;
+                return hasil;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //secara default
+                //throw new Exception(ex.Message) // pesan error secara default
+                //throw new FaultException("pesan salah") // untuk meloloskan pesan yang dikirim ke client
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Input yang Anda Masukan Salah";
+                mf.Pesan = "Masukan Input yang Benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+    }
+}
